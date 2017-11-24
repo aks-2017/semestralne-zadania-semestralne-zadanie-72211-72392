6c543da275db4ed8c68cc29fa05cdb3b2053f49f
Michal Skuta <michal.skuta@gmail.com>
2017-11-24 16:08:55 +0100

    added delays to runner scripts

--- ryu/runner3.py
+++ ryu/runner3.py
@@ -2,12 +2,17 @@
 
 from mininet.cli import CLI
 from mininet.net import Mininet
+from mininet.link import TCLink
 from mininet.node import RemoteController
 from mininet.term import makeTerm
+from time import sleep
+import subprocess
 
 if '__main__' == __name__:
-    net = Mininet(controller=RemoteController)
+    net = Mininet(controller=RemoteController, link=TCLink)
 
+    # set delay to communication with controller
+    subprocess.Popen(["sudo", "tc", "qdisc", "add", "dev", "lo", "root", "netem", "delay", "13ms"])
     c0 = net.addController('c0', port=6633)
 
     s1 = net.addSwitch('s1', protocols='OpenFlow13')
@@ -24,19 +29,19 @@ if '__main__' == __name__:
     h5 = net.addHost('h5')
     h6 = net.addHost('h6')
 
-    net.addLink(s1, h1)
-    net.addLink(s2, h2)
-    net.addLink(s3, h3)
-    net.addLink(s4, h4)
-    net.addLink(s5, h5)
-    net.addLink(s6, h6)
+    net.addLink(s1, h1, bw=100, delay='8ms')
+    net.addLink(s2, h2, bw=100, delay='8ms')
+    net.addLink(s3, h3, bw=100, delay='8ms')
+    net.addLink(s4, h4, bw=100, delay='8ms')
+    net.addLink(s5, h5, bw=100, delay='8ms')
+    net.addLink(s6, h6, bw=100, delay='8ms')
 
-    net.addLink(s1, s2)
-    net.addLink(s2, s3)
-    net.addLink(s3, s4)
-    net.addLink(s4, s5)
-    net.addLink(s5, s6)
-    net.addLink(s6, s1)
+    net.addLink(s1, s2, bw=100, delay='8ms')
+    net.addLink(s2, s3, bw=100, delay='8ms')
+    net.addLink(s3, s4, bw=100, delay='8ms')
+    net.addLink(s4, s5, bw=100, delay='8ms')
+    net.addLink(s5, s6, bw=100, delay='8ms')
+    net.addLink(s6, s1, bw=100, delay='8ms')
 
     net.build()
     c0.start()
